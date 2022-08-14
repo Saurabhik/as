a039bab02a37016d5625cfc68f44f3710f0b8fd4
saurabh singh <amreshsingh902681@gmail.com>
2022-08-14 10:50:13 +0530

    some message in b.java

--- b.java
+++ b.java
@@ -1,4 +1,13 @@
 public class sum {
 
+   public static void main (string [] args) {
+                int a=1;
+                int b=4;
+                system.out.println("the sum is" +(a+b));
+
+   }
+
+
+
     
 }
